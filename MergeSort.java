0060f213bd23b29cf6b7afb38e62195e4ece1bb6
Breno <brenodiasprotz@gmail.com>
2023-09-10 20:26:55 -0300

    first commit

---
+++ MergeSort.java
@@ -0,0 +1,94 @@
+public class MergeSort {
+
+    public static void merge(int[] array, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = array[left + i];
+        }
+
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = array[middle + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int middle = left + (right - left) / 2;
+
+            mergeSort(array, left, middle);
+            mergeSort(array, middle + 1, right);
+
+            merge(array, left, middle, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Uso: java MergeSort <elemento1> <elemento2> ...");
+            return;
+        }
+
+        int[] array = new int[args.length];
+
+        for (int i = 0; i < args.length; i++) {
+            if (isInteger(args[i])) {
+                array[i] = Integer.parseInt(args[i]);
+            } else {
+                System.out.println("Erro: \"" + args[i] + "\" não é um número inteiro válido.");
+                return;
+            }
+        }
+
+        System.out.println("Array antes do merge sort:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+
+        mergeSort(array, 0, array.length - 1);
+
+        System.out.println("\nArray depois do merge sort:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+Para rodar o programa você precisa de uma JDK instalada no linux versão 11 ou superior
+
+Passo 1: Verificar se o JDK está instalado
+Abra o terminal e digite o seguinte comando para verificar se o JDK está instalado:
+
+	java -version
+
+
+Se você não receber uma mensagem de erro e ver a versão do Java instalada, significa que o JDK está instalado. Caso contrário, você precisará instalar o JDK primeiro.
+
+Passo 2: Compilar o código Java
+No terminal, navegue para o diretório onde o arquivo Java está localizado usando o comando cd. Em seguida, compile o código Java usando o seguinte comando:
+
+	javac MeuPrograma.java
+
+
+Passo 3: Executar o programa Java
+Ainda no terminal e no mesmo diretório, execute o programa Java compilado usando o seguinte comando:
+
+	java MergeSort 12 11 13 5 6 7
+
+Os números em questão devem ser substituidos pelo seu vetor, e devem possuir espaços entre si,
